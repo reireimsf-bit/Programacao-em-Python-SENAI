e5c81316830e5b00c8a54980a3aee3444b80fbc7
reireimsf-bit <reireimsf@gmail.com>
2026-06-17 15:06:04 -0300

    Atividade da aula 5

---
+++ exercicios_aula5.py
@@ -0,0 +1,35 @@
+# 1
+print("Bem vindo!")
+
+# 2
+boolvar = True
+print(type(boolvar))
+
+# 3
+print(10.5 * 2.2)
+
+# 4
+print(90 / 3)
+
+# 5
+print(30 - 15)
+
+# 6
+print(6 // 4)
+
+# 7
+print(15.5 * 2.3 * 4.2 * 10.32)
+
+# 8
+numero = 10
+print(numero * 2)
+
+# 9
+nome = input("Nome:")
+email = input("E-Mail:")
+idade = int(input("Idade:"))
+cidade = input("Cidade:")
+grad = input("Graduação:")
+estcivil = input("Estado Civil:")
+
+print(f"Nome: {nome} E-mail: {email} Idade: {idade} Cidade: {cidade} Graduação: {grad} Estado Civil: {estcivil}")
