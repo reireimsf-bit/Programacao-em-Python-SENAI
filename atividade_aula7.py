03756ccb9ee16ee96d81ee9c9df0167f2efce461
reireimsf-bit <reireimsf@gmail.com>
2026-06-18 15:44:42 -0300

    Exercícios de Lista - Aula 7

---
+++ atividade_aula7.py
@@ -0,0 +1,28 @@
+# 0
+
+pares = list(range(0,21,2))
+print(pares)
+
+# 1
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
+print(numeros)
+
+# 2
+
+print(numeros[3])
+
+# 3
+
+numeros.append(9)
+print(numeros)
+
+# 4
+
+numeros.remove(5)
+print(numeros)
+
+# 5
+
+carros = ["Chevrolet", "Volkwagen", "BYD"]
+print(f"{numeros[0]} - {carros[0]} \n{numeros[1]} - {carros[1]} \n{numeros[2]} - {carros[2]}")
