03f64a7c4f422ed99d4d52f62031f14ecbf9e837
reireimsf-bit <reireimsf@gmail.com>
2026-06-25 17:53:54 -0300

    Atividades de Try e Except

---
+++ atividade_try-except.py
@@ -0,0 +1,20 @@
+# 1
+try:
+    num1 = int(input("Digite um número inteiro: "))
+    print(f"{num1} é um número inteiro.\n")
+except ValueError:
+    print("Digite apenas números inteiros!\n")
+
+# 2
+
+try:
+    num2 = float(input("Digite o dividendo: "))
+    num3 = float(input("Digite o divisor: "))
+    div = num2 / num3
+    print(div)
+except ZeroDivisionError:
+    print("Erro: Divisor não pode ser zero.")
+
+# 3
+
+lista = ["banana", "mamão", "limão"]
